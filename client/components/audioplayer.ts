d90398c1a8630b97aa2aca6272d6d91bed1e3ce3
Geoff Kimball <geoff@zurb.com>
2016-01-24 13:36:56 -0800

    Add progress bar to audio player that fills as the track plays

--- client/components/audioplayer.ts
+++ client/components/audioplayer.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, AfterViewInit } from 'angular2/core';
+import { ngStyle } from 'angular2/common';
 import TrackQueueService from '../services/trackqueue';
 import { Rooms } from '../../collections/rooms';
 
@@ -8,7 +9,8 @@ declare var $;
   selector: '.audioPlayer',
   template: `
     <div *ngIf="track" class="audioPlayer_wrapper">
-      <audio src="{{ track.stream_url }}?client_id={{ clientID }}" (canplay)="player.play()" (ended)="onTrackEnd()" #player></audio>
+      <audio src="{{ track.stream_url }}?client_id={{ clientID }}" (canplay)="player.play()" (ended)="onTrackEnd()" (timeupdate)="calculateProgressBar()" #player></audio>
+      <div class="audioPlayer_progressBar" [ngStyle]="{'transform': progressBarStyle}"></div>
       <img [src]="track.artwork_url" class="audioPlayer_image">
       <button type="button" (click)="togglePlayState()" class="audioPlayer_button fa" [class.fa-play-circle]="player.paused" [class.fa-pause-circle]="!player.paused">{{ player.paused ? 'Play' : 'Pause' }}</button>
       <div class="audioPlayer_text">
@@ -21,11 +23,13 @@ declare var $;
 export default class AudioPlayer implements AfterViewInit {
   paused: boolean;
   clientID: string;
+  progressBarStyle: string;
   player;
   track;
 
   constructor(private elem: ElementRef) {
     this.clientID = Meteor.settings.public.soundcloudID;
+    this.progressBarStyle = 'scaleX(0)';
 
     Tracker.autorun(zone.bind(() => {
       let room = Rooms.findOne(1);
@@ -44,6 +48,12 @@ export default class AudioPlayer implements AfterViewInit {
     else {
       this.player.pause();
     }
+  }
+
+  calculateProgressBar(): void {
+    let value = this.player.currentTime / this.player.duration;
+
+    this.progressBarStyle = `scaleX(${value})`;
   }
 
   onTrackEnd() {}
