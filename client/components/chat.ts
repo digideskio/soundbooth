143c3ed4d47a8e7b64a7a695208977c13e4b74fd
Geoff Kimball <geoff@zurb.com>
2016-01-25 22:03:20 -0800

    Add metadata field to chat messages that shows what song was playing if some expresses taste/distate for it

--- client/components/chat.ts
+++ client/components/chat.ts
@@ -9,6 +9,7 @@ import { Rooms } from '../../collections/rooms';
       <p class="message_author">{{ message.author }}</p>
       <time class="message_time">{{ message.time | date:'shortTime' }}</time>
       <p class="message_body">{{ message.message }}</p>
+      <p class="message_meta" *ngIf="message.meta">{{ message.meta }}</p>
     </div>
     <textarea (keyup.enter)="sendMessage()" [(ngModel)]="chatbox" class="chat_messageBox"></textarea>
   `
@@ -30,6 +31,12 @@ export default class Chat {
       message: this.chatbox,
       time: Date.now(),
       author: Meteor.user().profile.name
+    }
+
+    if (message.message.toLowerCase().match(/(track|trak|suck|awful|love|hate|this)/)) {
+      let song = this.room.activeTrack.title;
+      let artist = this.room.activeTrack.user.username;
+      message.meta = `Said during "${song}" by ${artist}`;
     }
 
     Meteor.call('postMessage', 1, message, (err, res) => {
